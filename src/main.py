c6682b484c7ce3f6386c979e08119766ad2d66f2
zuegp <pazueger@hotmail.com>
2026-04-07 22:19:55 +0200

    some error found

--- src/main.py
+++ src/main.py
@@ -17,8 +17,8 @@ def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
-        print("Copying static files to public directory...")
-        copy_files_recursive(dir_path_static, dir_path_public)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
     
     print("Generating content...")
     generate_pages_recursive(basepath, dir_path_content, template_path, dir_path_public)
